0d659d7395bee39c02c73dc899ef0103841eafe7
artursanntos <arturcs2001@gmail.com>
2023-12-29 17:57:48 -0300

    add register functionality Update client package.json and app component and create interface

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
     selector: 'app-root',
@@ -9,18 +10,20 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
     
     title = 'DatingApp';
-    users: any;
 
-    constructor(private http: HttpClient) {
+    constructor(private accountService: AccountService) {
     
     }
 
     ngOnInit(): void { // Isso é um método do ciclo de vida do Angular, é executado quando o componente é inicializado
-        this.http.get('https://localhost:5001/api/users').subscribe({
-            next: response => this.users = response,
-            error: error => console.log(error),
-            complete: () => console.log('Request Completed, users: ', this.users)
-        });
+        this.setCurrentUser();
     };
+
+    setCurrentUser() {
+        const userString = localStorage.getItem('user');
+        if (!userString) return;
+        const user: User = JSON.parse(userString);
+        this.accountService.setCurrentUser(user);
+    }
 
 }
