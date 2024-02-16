110f341d728ccb87019081a2701e85522521f056
artursanntos <arturcs2001@gmail.com>
2024-02-16 15:51:33 -0300

    Add member-card component and update styles

--- client/src/app/members/members-detail/members-detail.component.ts
+++ client/src/app/members/members-detail/members-detail.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Member } from '../../_models/member';
+import { MembersService } from '../../_services/members.service';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 @Component({
   selector: 'app-members-detail',
+  standalone: true,
   templateUrl: './members-detail.component.html',
-  styleUrl: './members-detail.component.css'
+  styleUrl: './members-detail.component.css',
+  imports: [
+    CommonModule,
+    TabsModule,
+    GalleryModule,
+  ]
 })
-export class MembersDetailComponent {
+export class MembersDetailComponent implements OnInit {
+
+  member: Member | undefined;
+  images: GalleryItem[] = [];
+
+  constructor(private memberService: MembersService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.loadMember();
+  }
+
+  loadMember() {
+    var username = this.route.snapshot.paramMap.get('username');
+    if (!username) return;
+    this.memberService.getMember(username).subscribe({
+      next: member => {
+        this.member = member,
+        this.getImages();
+      }
+    })
+  }
+
+  getImages() {
+    if (!this.member || !this.member.photos) return;
+    for (const photo of this.member.photos) {
+      this.images.push(new ImageItem({ src: photo.url, thumb: photo.url }));
+    }
+  }
 
 }
